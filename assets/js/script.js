5afbb6ae3d554976c9f5fd471c811dff0c20a725
Ciaran Griffin <griffinciaran3@gmail.com>
2024-02-13 10:59:39 +0000

    Add const questions in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,104 @@
-console.log("connected!")
+// Const Questions 
+
+const Questions = [
+  {
+    question: "When was Liverpool Fc founded?",
+    answers: [
+      { text:'1890', correct: false }
+      { text: '1892', correct: true }
+      { text: '1890', correct: false }
+      { text: '1982', correct: false }
+    ]
+  },
+
+  {
+    question: "Who was the first owner of Liverpool Fc?",
+    answers: [
+      { text:'John Houlding', correct: true }
+      { text: 'Tom Werner', correct: false }
+      { text: 'John William', correct: false }
+      { text: 'Bill Hogan', correct: false }
+    ]
+  },
+
+  {
+    question: "What year did the Haysel stadium disaster happen?",
+    answers: [
+      { text:'1980', correct: false }
+      { text: '1982', correct: false }
+      { text: '1982', correct: true }
+      { text: '1988', correct: false }
+    ]
+  },
+
+  {
+    question: "What is the name of Liverpool Fc home ground?",
+    answers: [
+      { text:'Old Trafford', correct: false }
+      { text: 'Anfield', correct: true }
+      { text: 'Emirates Staduim', correct: false }
+      { text: 'Croke park', correct: false }
+    ]
+  },
+
+  {
+    question: "What is Liverpool Fc Anthem?",
+    answers: [
+      { text:'Glory Glory', correct: false }
+      { text: "You'll Never Walk Alone", correct: true }
+      { text: 'Celtic', correct: false }
+      { text: 'Come on the Red', correct: false }
+    ]
+  },
+
+  {
+    question: "Who was Liverpool first captain?",
+    answers: [
+      { text:'Jimmy Ross', correct: false }
+      { text: 'Steven Gerrard', correct: false }
+      { text: 'Andrew Hennah', correct: true }
+      { text: 'Jordan Henderson', correct: false }
+    ]
+  },
+
+  {
+    question: "How Many times has Liverpool won the Premier League?",
+    answers: [
+      { text:'15', correct: false }
+      { text: '17', correct: false }
+      { text: '19', correct: true }
+      { text: '21', correct: false }
+    ]
+  },
+
+  {
+    question: "How many FA cups have Liverpool won?",
+    answers: [
+      { text:'7', correct: false }
+      { text: '8', correct: true }
+      { text: '5', correct: false }
+      { text: '4', correct: false }
+    ]
+  },
+
+  {
+    question: "How many times Liverpool won Champions League?",
+    answers: [
+      { text:'3', correct: false }
+      { text: '7', correct: false }
+      { text: '6', correct: true }
+      { text: '9', correct: false }
+    ]
+  },
+
+  {
+    question: "Who did Liverpool beat in the 2005 Champions League",
+    answers: [
+      { text:'Ac Millan', correct: true }
+      { text: 'Ajax', correct: false }
+      { text: 'Real Madrid', correct: false }
+      { text: 'Batcalona', correct: false }
+    ]
+  },
+
+];
