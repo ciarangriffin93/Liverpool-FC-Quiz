cb5f663fb8ad1f3db66eff75708684a0e53976b5
Ciaran Griffin <griffinciaran3@gmail.com>
2024-02-20 15:43:22 +0000

    changed the questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,134 +1,110 @@
 // Questions 
-
-let questions = [
+"use strict"
+const questions = [
   {
     question: "When was Liverpool Fc founded?",
-    answers: [
-      { text:"1890", correct: false },
-      { text: "1892", correct: true },
-      { text: "1890", correct: false },
-      { text: "1982", correct: false },
-    ]
+    a: "1980",
+    b: "1892",
+    c: "1890",
+    d: "1982",
+    correct: "b",
   },
 
   {
     question: "Who was the first owner of Liverpool Fc?",
-    answers: [
-      { text: "John Houlding", correct: true },
-      { text: "Tom Werner", correct: false },
-      { text: "John William", correct: false },
-      { text: "Bill Hogan", correct: false },
-    ]
+  
+    a: "John Houlding", 
+    b: "Tom Werner",
+    c: "John William",
+    d: "Bill Hogan",
+    correct: "a",
   },
 
   {
     question: "What year did the Haysel stadium disaster happen?",
-    answers: [
-      { text: "1980", correct: false },
-      { text: "1982", correct: false },
-      { text: "1982", correct: true },
-      { text: "1988", correct: false },
-    ]
+    
+    a: "1980",
+    b: "1982",
+    c: "1985", 
+    d: "1988",
+    correct: "c"
+    
   },
 
   {
     question: "What is the name of Liverpool Fc home ground?",
-    answers: [
-      { text: "Old Trafford", correct: false },
-      { text: "Anfield", correct: true },
-      { text: "Emirates Staduim", correct: false },
-      { text: "Croke park", correct: false },
-    ]
+  
+    a:"Old Trafford",
+    b:"Anfield",
+    c:"Emirates Staduim", 
+    d:"Croke park",
+    correct: "b",
   },
 
   {
     question: "What is Liverpool Fc Anthem?",
-    answers: [
-      { text: "Glory Glory", correct: false },
-      { text: "You'll Never Walk Alone", correct: true },
-      { text: "Celtic", correct: false },
-      { text: "Come on the Red", correct: false },
-    ]
+
+    a:"Glory Glory",
+    b:"You'll Never Walk Alone", 
+    c:"Celtic", 
+    d:"Come on the Red",
+    correct: "b",
+    
   },
 
   {
     question: "Who was Liverpool first captain?",
-    answers: [
-      { text: "Jimmy Ross", correct: false },
-      { text: "Steven Gerrard", correct: false },
-      { text: "Andrew Hennah", correct: true },
-      { text: "Jordan Henderson", correct: false },
-    ]
+    
+    a: "Jimmy Ross",
+    b: "Steven Gerrard", 
+    c: "Andrew Hennah", 
+    d: "Jordan Henderson",
+    correct: "c",
+    
   },
 
   {
     question: "How Many times has Liverpool won the Premier League?",
-    answers: [
-      { text: "15", correct: false },
-      { text: "17", correct: false },
-      { text: "19", correct: true },
-      { text: "21", correct: false },
-    ]
+  
+    a: "15",
+    b: "17", 
+    c: "19",
+    d: "21",
+    correct: "c",
+    
   },
 
   {
     question: "How many FA cups have Liverpool won?",
-    answers: [
-      { text: "7", correct: false },
-      { text: "8", correct: true },
-      { text: "5", correct: false },
-      { text: "4", correct: false },
-    ]
+    
+    a: "7", 
+    b: "8", 
+    c: "5", 
+    d: "4",
+    correct: "b",
+    
   },
 
   {
     question: "How many times Liverpool won Champions League?",
-    answers: [
-      { text: "3", correct: false },
-      { text: "7", correct: false },
-      { text: "6", correct: true },
-      { text: "9", correct: false },
-    ]
+    
+    a: "3", 
+    b: "7",
+    c: "6",
+    d: "9",
+    correct: "c",
+    
   },
 
   {
     question: "Who did Liverpool beat in the 2005 Champions League",
-    answers: [
-      { text: "Ac Millan", correct: true },
-      { text: "Ajax", correct: false },
-      { text: "Real Madrid", correct: false },
-      { text: "Batcalona", correct: false },
-    ]
+    
+    a: "Ac Millan",
+    b: "Ajax", 
+    c: "Real Madrid",
+    d: "Batcalona", 
+    correct: "a",
+    
   }
 
 ];
-
-const questtion = document.getElementById("question")
-const answerBtn = document.getElementById("answer-btn")
-const userScore = document.getElementById("user-score")
-const totalScore = document.getElementById("total-score")
-const prevBtn = document.getElementById("prev-btn")
-const nextBtn = document.getElementById("next-btn")
-
-prevBtn.addEventListener("click",prev);
-nextBtn.addEventListener("click",next);
-
-function startQuiz()
-
-
-function showQuestion()
-
-
-function resetState()
-
-
-function questionAnswer()
-
-
-function NextButton()
-
-
-function showScore()
-
-
-startQuiz()
