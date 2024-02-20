09132a5d28f3a6a5c70e842a362e75509047116b
Ciaran Griffin <griffinciaran3@gmail.com>
2024-02-20 16:51:09 +0000

    Add function quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -110,6 +110,7 @@ const questions = [
 ];
 
 //ref to html
+
 const quiz = document.querySelector(".quiz-area");
 const answerEl = document.querySelectorAll(".answer");
 const questionEl = document.getElementById("question");
@@ -120,4 +121,22 @@ const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
+
+//question arry
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz(){
+  const currentQuestion = questions[currentQuiz];
+}
+
+
+
+
+
+
+
 
